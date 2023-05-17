1d987dcbd2139d5ac770d9701cd49173fb77da91
Michael Perkins <mperkins1995@gmail.com>
2023-05-17 15:24:01 -0500

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name:"Jim Halpert",
+    streetAddress:"111 Scranton Ave"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeCopy = {...employee};
+    employeeCopy[key]=value;
+
+    return employeeCopy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeCopy = {...employee};
+    delete employeeCopy[key];
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+}
